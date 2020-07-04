78e7f838a5599ad2ea527410fe2cf74a0d3eaf33
Efren <efren.valdez@kotahi.co.nz>
2020-07-04 22:03:29 +1200

    initial

---
+++ Invoice.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InvoiceProject
+{
+    public class Invoice
+    {
+        public int InvoiceNumber { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public List<InvoiceLine> LineItems = new List<InvoiceLine>{};
+
+        public void AddInvoiceLine(InvoiceLine invoiceLine)
+        {
+            LineItems.Add(invoiceLine);
+        }
+
+        public void RemoveInvoiceLine(int SOMEID)
+        {
+            LineItems.RemoveAt(SOMEID);
+        }
+
+        /// <summary>
+        /// GetTotal should return the sum of (Cost * Quantity) for each line item
+        /// </summary>
+        public decimal GetTotal()
+        {
+            return (Decimal)LineItems.Sum(item => item.Cost * item.Quantity);
+        }
+
+        /// <summary>
+        /// MergeInvoices appends the items from the sourceInvoice to the current invoice
+        /// </summary>
+        /// <param name="sourceInvoice">Invoice to merge from</param>
+        public void MergeInvoices(Invoice sourceInvoice)
+        {
+            foreach (InvoiceLine lineItems in sourceInvoice.LineItems) {
+                LineItems.Add(lineItems);
+            }
+        }
+
+        /// <summary>
+        /// Creates a deep clone of the current invoice (all fields and properties)
+        /// </summary>
+        public Invoice Clone()
+        {
+            return new Invoice() {
+                InvoiceNumber = InvoiceNumber,
+                InvoiceDate = InvoiceDate,
+                LineItems = LineItems
+            };
+        }
+
+        /// <summary>
+        /// Outputs string containing the following (replace [] with actual values):
+        /// Invoice Number: [InvoiceNumber], InvoiceDate: [dd/MM/yyyy], LineItemCount: [Number of items in LineItems]
+        /// </summary>
+        public override string ToString()
+        {
+            string invoiceNumber = InvoiceNumber.ToString();
+            string invoiceDate = InvoiceDate.ToString("dd/MM/yyyy");
+            string lineItems = LineItems.Count().ToString();
+            return $"Invoice Number: {invoiceNumber}, InvoiceDate: {invoiceDate}, LineItemCount: {lineItems}";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,170 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace InvoiceProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Xero Tech Test!");
+
+            CreateInvoiceWithOneItem();
+            CreateInvoiceWithMultipleItemsAndQuantities();
+            RemoveItem();
+            MergeInvoices();
+            CloneInvoice();
+            InvoiceToString();
+        }
+
+        private static void CreateInvoiceWithOneItem()
+        {
+            var invoice = new Invoice();
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 1,
+                Cost = 6.99,
+                Quantity = 1,
+                Description = "Apple"
+            });
+
+            Console.WriteLine(invoice.GetTotal());
+        }
+
+        private static void CreateInvoiceWithMultipleItemsAndQuantities()
+        {
+            var invoice = new Invoice();
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 1,
+                Cost = 10.21,
+                Quantity = 4,
+                Description = "Banana"
+            });
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 2,
+                Cost = 5.21,
+                Quantity = 1,
+                Description = "Orange"
+            });
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 3,
+                Cost = 5.21,
+                Quantity = 5,
+                Description = "Pineapple"
+            });
+
+            Console.WriteLine(invoice.GetTotal());
+        }
+
+        private static void RemoveItem()
+        {
+            var invoice = new Invoice();
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 1,
+                Cost = 5.21,
+                Quantity = 1,
+                Description = "Orange"
+            });
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 2,
+                Cost = 10.99,
+                Quantity = 4,
+                Description = "Banana"
+            });
+
+            invoice.RemoveInvoiceLine(1);
+            Console.WriteLine(invoice.GetTotal());
+        }
+
+        private static void MergeInvoices()
+        {
+            var invoice1 = new Invoice();
+
+            invoice1.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 1,
+                Cost = 10.33,
+                Quantity = 4,
+                Description = "Banana"
+            });
+
+            var invoice2 = new Invoice();
+
+            invoice2.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 2,
+                Cost = 5.22,
+                Quantity = 1,
+                Description = "Orange"
+            });
+
+            invoice2.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 3,
+                Cost = 6.27,
+                Quantity = 3,
+                Description = "Blueberries"
+            });
+
+            invoice1.MergeInvoices(invoice2);
+            Console.WriteLine(invoice1.GetTotal());
+        }
+
+        private static void CloneInvoice()
+        {
+            var invoice = new Invoice();
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 1,
+                Cost = 6.99,
+                Quantity = 1,
+                Description = "Apple"
+            });
+
+            invoice.AddInvoiceLine(new InvoiceLine()
+            {
+                InvoiceLineId = 2,
+                Cost = 6.27,
+                Quantity = 3,
+                Description = "Blueberries"
+            });
+
+            var clonedInvoice = invoice.Clone();
+            Console.WriteLine(clonedInvoice.GetTotal());
+        }
+
+        private static void InvoiceToString()
+        {
+            var invoice = new Invoice()
+            {
+                InvoiceDate = DateTime.Now,
+                InvoiceNumber = 1000,
+                LineItems = new List<InvoiceLine>()
+                {
+                    new InvoiceLine()
+                    {
+                        InvoiceLineId = 1,
+                        Cost = 6.99,
+                        Quantity = 1,
+                        Description = "Apple"
+                    }
+                }
+            };
+
+            Console.WriteLine(invoice.ToString());
+        }
+    }
+}
